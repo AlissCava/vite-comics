7adf27617de63f6b9a21d40c63459e4a09c50ea2
AlissCava <cavalleraalissa@gmail.com>
2023-12-21 21:46:32 +0100

    forse ci sto capendo qualcosa., abozzo di herder

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<script>
+    export default {
+        name: "AppHeader",
+    }
+</script>
+<template>
+  <header>
+    <div>
+        <img src=" /img/dc-logo.png">
+    </div>
+
+   
+    <div id="navbar">
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+        <a href="/">CHARACTER</a>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+header{
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+   
+    #navbar{
+        list-style: none;
+        margin: 2.5rem 1rem 4rem;
+
+        a {
+            color: rgb(0, 0, 0);
+            display: inline-block;
+            padding: 1rem;
+            text-decoration: none;
+            font-weight: 600;
+        }
+    }
+}
+
+</style>
